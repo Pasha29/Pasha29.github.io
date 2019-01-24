96a4d2344db53404b3c4562628f0570893ad3748
Pasha29 <pasha4383@gmail.com>
2019-01-24 23:49:46 +0200

    Our team

---
+++ js/ourTeamJS.js
@@ -0,0 +1,28 @@
+
+$(".infoCard").hide();
+
+// var names = document.getElementsByClassName("q1");
+ // alert(names.length);
+
+  // $(".card-img-top").on({
+  //  "mouseover" : function() {
+  //    $(".card-img-top").slideUp("slow");
+  //    $(".infoCard").slideDown("slow");
+  // }
+  // });
+  //
+  // $(".infoCard").on({
+  // "mouseover" : function() {
+  //   // $(".infoCard").slideUp("slow");
+  //   $(".card-img-top").slideDown("slow");
+  //   $(".infoCard").slideUp("slow");
+  //   // $("infoCard").hide();
+  // }
+  // });
+
+   // },
+   //
+   // "mouseout" : function() {
+   //   alert("2");
+   //   // $("infoCard").hide();
+   // }
