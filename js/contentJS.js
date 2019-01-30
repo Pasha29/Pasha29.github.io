cb0dbba3f35436ef450632fc271703a694860ceb
Pasha29 <pasha4383@gmail.com>
2019-01-30 23:15:53 +0200

    Production не доделал

---
+++ js/contentJS.js
@@ -0,0 +1,19 @@
+// $(".card-body").hide();
+//
+// $(".card").on({
+//  "mouseover" : function() {
+//    $(".card-body").slideDown("slow");
+// }
+// });
+//
+// $(".card").on({
+// "mouseover" : function() {
+//   // $(".infoCard").slideUp("slow");
+//   $(".card-body").slideUp("slow");
+//   // $("infoCard").hide();
+// }
+// });
+
+$(".card").click(function(){
+  window.location = "contentInfo.html";
+});
---
+++ js/productionJS.js
@@ -0,0 +1,28 @@
+ // $(".advantages").hide();
+
+
+// function onloadPage(){
+
+var logo = $(".logo").width();
+var widthBody = $(window).width();
+// var heightBody = $(window).height();
+
+var horizontalCenterLogo = (widthBody / 2) - (logo / 2);
+// var verticalCenterLogo = (heightBody / 2) - (logo / 2);
+
+// alert(heightBody);
+// $(".logo").css({"position": "absolute", "left": horizontalCenterLogo+"px", "top": verticalCenterLogo+"px"});
+$(".logo").css({"position": "absolute", "left": horizontalCenterLogo+"px"});
+
+//}
+
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * Math.floor(max));
+// }
+// $(".advantages2").css({
+//   "position": "absolute",
+//    "left": getRandomInt(widthBody / 2)+"px",
+//    "right": getRandomInt(widthBody / 2)+"px",
+//    "top": getRandomInt(heightBody / 2)+"px",
+//    "bottom": getRandomInt(heightBody / 2)+"px",
+//  });
