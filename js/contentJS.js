7145949fd4eacfea6197494e040c7113e7968edb
Pasha29 <pasha4383@gmail.com>
2019-03-28 12:57:32 +0200

    new content

--- js/contentJS.js
+++ js/contentJS.js
@@ -1,149 +1,84 @@
-// $(".card-body").hide();
+//открытие блоков
+
+
+////////////////////////////////
+
+/*
+
+<td>
+  <div class="card">
+      <img src="http://www.mkmediagroup.tv/wp-content/uploads/2017/07/Whos_there-upd-1-uai-516x344.jpg"
+       class="card-img-top"> <!-- onmouseover="openBlock(this);"-->
+      <div class="card-body">
+        <p class="card-text">
+        <h6><u>Name of content</u></h6><span class="content_name"></span>
+        <h6><u>Count and duration of content</u></h6><span class="episode_count_duration"></span>
+        <h6><u>Year</u></h6> <span class="episode_year"></span>
+        <h6><u>Genre</u></h6>	<span class="episode_genre"></span>
+        <h6><u>Audience</u></h6> <span class="episode_audience"></span>
+        <h6><u>Language</u></h6> <span class="episode_language"></span>
+        <h6><u>Format</u></h6> <span class="episode_format"></span>
+        </p>
+      </div>
+  </div>
+</td>
+
+*/
+
 //
-// $(".card").on({
-//  "mouseover" : function() {
-//    $(".card-body").slideDown("slow");
-// }
-// });
-//
-// $(".card").on({
-// "mouseover" : function() {
-//   // $(".infoCard").slideUp("slow");
-//   $(".card-body").slideUp("slow");
-//   // $("infoCard").hide();
-// }
-// });
+$(document).ready(function() {
+  $.ajax({
+      url: "http://35.157.157.111:8080/api/content"
+  }).done(function(data) {
+    console.log(data);
+    $(function() {
+      $.each(data, function(i, item) {
+        if(item.pictureLink == null){
+          return;
+        }
+          var $div = $('<tr>').append('<td><div class="card"><img class="card-img-top" src="' + item.pictureLink + '"><div class="card-body"><p class="card-text"><h6><u>Name of content</u></h6><span>' + item.contentName + '</span></p></div></div></td>')
+          .appendTo('#contentTable');
 
-////////////////////////////////////////////////////////////////////////////////
+                });
+                $('.card').on("mouseover",function() {
+                 $('.card-body', this).stop().slideDown(1000, 'linear');
+                });
 
-//стрелки
-
-// $('.arrow').css('opacity', '0');
-//
-//   setInterval(function(){
-//     if()
-//   $('.logo').css('opacity', '0');
-//   $('.arrow').css('opacity', '1');
-// }, 10000);
-//
-// setInterval(function(){
-// $('.logo').css('opacity', '1');
-// $('.arrow').css('opacity', '0');
-// }, 7000);
-
-/* flipInY
-flipOutY */
-  ///////////////////////////////////////////////////////////////////
-
-  //попытки
-
-  // var kids = document.querySelectorAll('.card-body');
-  // console.log(kids);
-
-// $('.card-body').hide();
-
-// $('.card').on("mouseover", function(e){
-//   var a = e.target;
-//     openInfo(a);
-//  });
-//
-//
-//  $('.card').on("mouseout", function(e){
-//    var a = e.target;
-//     closeInfo(a);
-//   });
-//
-//
-//     function openInfo(a){
-//       console.log(a.childNodes);
-//       for(var i = 0; i < a.childNodes.length; i++){
-//         console.log(a[0].childNodes);
-//       // if(a.childNodes.className == 'card-body'){
-//         // $(a.childNodes).show();
-//         // console.log("1");
-//       }
-//     }
-//     function closeInfo(a){
-//       if(a.className == 'card-body'){
-//         // $(a).hide();
-//       }
-//     }
+                $('.card').on("mouseout",function() {
+                 $('.card-body', this).stop().slideUp(2000, 'linear');
+                });
 
 
-        // $(kids[0]).on("mouseover", function(){
-           // for(var i = 0; i < kids.length; i++){
-             // if(kids[0].className == 'card'){
-             // console.log(this + "навёл на карточку");
-          //   for(var i = 0; i < kids.length; i++){
-          //   $(kids[i].parentNode.childNodes).slideUp("slow");
-            // }
-          // }
-        // });
-  // .parentNode.childNodes;
-
-/////////////////////////////////////////////////////////////////////
-
-//рабочее с интернета
-
-// function openBlock(el) {
-//   var kids = el.parentNode.childNodes;
-// 	for (var i = 0; i < kids.length; i++) {
-// 		var child = kids[i];
-// 		if (child && child.className == "card-body") {
-// 			if (child.style.display != 'block') {
-// 				child.style.display = 'block';
-// 			} else {
-// 				child.style.display = 'none';
-// 			}
-// 		}
-// 	}
-// }
-
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-//для данных
-
-// $(document).ready(function() {
-//   $.ajax({
-//       url: "http://35.157.157.111:8080/api/content/1"
-//   }).done(function(data){
-//     $('.content_name').append(data.contentName);
-//     $('.episode_count_duration').append(data.episosesCount + " " + data.episodesDuration);
-//     $('.episode_year').append(data.year);
-//     $('.episode_audience').append(data.audience.audienceAge);
-//     // $('.content_name').append(data.language);
-//     $('.content_name').append(data.format);
-//   });
-// });
-
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-var a = $('.card').length;
-console.log(a);
-var i;
-for (i = 0; i < a; i++) {
-  // setTimeout(function(){
-    console.log(i);
-    $('.card').eq(i).addClass("animated wow bounceInRight");
-  // }, 1000);
-}
-
-
-        // $('.card').eq(i).addClass('animated wow bounceInRight');
-
-
-$(".card-img-top").click(function(){
-  window.location = "contentInfo.html";
+                $(".card-img-top").click(function(){
+                  window.location = "contentInfo.html";
+                });
+          });
+        }
+    );
 });
 
 
-function turnDevice(){
-    if($("body").width() <= 499){
-      $(".cover-container").hide();
-      window.location = "turnDevice.html";
-    }
-    else if($("body").width() >= 500){
-      $(".cover-container").show();
-    }
-}
+
+
+// <h6><u>Count and duration of content</u></h6><span>' + + '</span>
+// <h6><u>Year</u></h6> <span>' + + '</span>
+// <h6><u>Genre</u></h6>	<span>' + + '</span>
+// <h6><u>Audience</u></h6> <span>' + + '</span>
+// <h6><u>Language</u></h6> <span>' + + '</span>
+// <h6><u>Format</u></h6> <span>' + + '</span>
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// /*var a = $('.card').length;
+// console.log(a);
+// var i;
+// for (i = 0; i < a; i++) {
+//   // setTimeout(function(){
+//     console.log(i);
+//     $('.card').eq(i).addClass("animated wow bounceInRight");
+//   // }, 1000);
+// }
+
+// $('.card').eq(i).addClass('animated wow bounceInRight');
+
+
+//страница дополнительной информации о контенте
--- js/ourTeamJS.js
+++ js/ourTeamJS.js
@@ -1,48 +1,13 @@
-$(".cover-container").hide();
+//открытие блоков
+//
+$('.infoCard').show();
 
-  $(".cover-container").fadeIn(2000, function(){
-    $(".cover-container").show();
-});
-
-
-// $('.cover-container').addClass('delay-1s');
-// function fadeWindow(){
-//   $(document.body).fadeIn(2000);
-// }
-// fadeWindow();
 
 //
-// $(".infoCard").show();
-// $(".card-img-top").hide();
-
-// $(".infoCard").hide();
-// $(".card-img-top").show();
-
-// var names = document.getElementsByClassName("q1");
- // alert(names.length);
-
-  //
-  //
-  // $(".card-img-top").on({
-  //  "mouseover" : function() {
-  //    $(".card-img-top").slideUp("slow");
-  //    $(".infoCard").slideDown("slow");
-  // }
-  // });
-  //
-  // $(".infoCard").on({
-  // "mouseover" : function() {
-  //   // $(".infoCard").slideUp("slow");
-  //   $(".card-img-top").slideDown("slow");
-  //   $(".infoCard").slideUp("slow");
-  //   // $("infoCard").hide();
-  // }
-  // });
-
-
-   // },
-   //
-   // "mouseout" : function() {
-   //   alert("2");
-   //   // $("infoCard").hide();
-   // }
+// $('.card').on("mouseover",function() {
+//  $(this).find('div').stop().slideDown(1000, 'linear');
+// });
+//
+// $('.card').on("mouseout",function() {
+//  $(this).find('div').stop().slideUp(1000, 'linear');
+// });
